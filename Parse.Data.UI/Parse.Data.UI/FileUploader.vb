94fcf0a97ed9d2599563ac20724b5f69fc4dea8e
Gooner <suneeshchummar@live.com>
2012-10-07 00:10:52 -0400

    Added try catch for service

--- Parse.Data.UI/Parse.Data.UI/FileUploader.vb
+++ Parse.Data.UI/Parse.Data.UI/FileUploader.vb
@@ -8,11 +8,15 @@ Public Class FileUploader
     Public Function UploadImage(fileName As String, ByVal fileContent As Stream) As String Implements IFileUploader.UploadImage
         Dim filePath = String.Format("~/App_Data/Image/{0}_{1}", Guid.NewGuid(), fileName)
 
-        Using destination = New FileStream(filePath, FileMode.Create)
-            fileContent.CopyTo(destination)
-            destination.Flush()
-            destination.Close()
-        End Using
+        Try
+            Using destination = New FileStream(filePath, FileMode.Create)
+                fileContent.CopyTo(destination)
+                destination.Flush()
+                destination.Close()
+            End Using
+        Catch ex As Exception
+            Return ex.Message
+        End Try
 
         Return filePath
     End Function
