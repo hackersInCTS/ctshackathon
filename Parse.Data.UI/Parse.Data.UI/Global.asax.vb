0305c811d5fa04bd9682a87933e7d58788826987
Gooner <suneeshchummar@live.com>
2012-10-07 10:05:54 -0400

    added mvc page to upload

--- Parse.Data.UI/Parse.Data.UI/Global.asax.vb
+++ Parse.Data.UI/Parse.Data.UI/Global.asax.vb
@@ -1,12 +1,28 @@
 ﻿Imports System.Web.SessionState
 Imports System.Web.Routing
 Imports System.ServiceModel.Activation
+Imports System.Web.Mvc
 
 Public Class Global_asax
     Inherits System.Web.HttpApplication
 
     Sub Application_Start(ByVal sender As Object, ByVal e As EventArgs)
-        RouteTable.Routes.Add(New ServiceRoute("FileUploader", New WebServiceHostFactory(), GetType(FileUploader)))
+        'RouteTable.Routes.Add(New ServiceRoute("FileUploader", New WebServiceHostFactory(), GetType(FileUploader)))
+        RegisterRoutes(RouteTable.Routes)
+    End Sub
+
+    Shared Sub RegisterGlobalFilters(ByVal filters As GlobalFilterCollection)
+        filters.Add(New HandleErrorAttribute())
+    End Sub
+
+    Shared Sub RegisterRoutes(ByVal routes As RouteCollection)
+        routes.IgnoreRoute("{resource}.axd/{*pathInfo}")
+
+        routes.MapRoute("Default", "{controller}/{action}/{id}",
+                        New With {.controller = "Home", .action = "Index", .id = UrlParameter.Optional})
+        routes.MapRoute("FileUpload", "{controller}/{action}",
+                       New With {.controller = "FileUpload", .action = "Index"})
+
     End Sub
 
     Sub Session_Start(ByVal sender As Object, ByVal e As EventArgs)
