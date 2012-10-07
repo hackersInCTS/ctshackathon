e3a135b780b76ef5aa93dcaa30ec448b9ce58928
Gooner <suneeshchummar@live.com>
2012-10-07 01:01:39 -0400

    Added page to view last added file

---
+++ Parse.Data.UI/Parse.Data.UI/GetLastImage.aspx.vb
@@ -0,0 +1,21 @@
+﻿Imports System.IO
+Imports System.Threading
+
+Public Class GetLastImage
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim folderPath = Server.MapPath(String.Format("~/App_Data/{0}/", Request("Directory")))
+        Dim lastUpdatedFile = (New DirectoryInfo(folderPath)).GetFiles().OrderByDescending(Function(file) file.LastWriteTime).FirstOrDefault()
+        If lastUpdatedFile IsNot Nothing Then
+            Try
+                Response.ContentType = "application/octet-stream"
+                Response.AppendHeader("Content-Disposition", "attachment; filename=" + lastUpdatedFile.Name)
+                Response.TransmitFile(lastUpdatedFile.FullName)
+                Response.End()
+            Catch ex As ThreadAbortException
+            End Try
+        End If
+    End Sub
+
+End Class
