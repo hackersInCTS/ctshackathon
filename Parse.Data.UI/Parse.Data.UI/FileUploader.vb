fe01623520b6fdb23225cd23c753c180561b6066
Gooner <suneeshchummar@live.com>
2012-10-07 00:46:32 -0400

    Added code to create directory

--- Parse.Data.UI/Parse.Data.UI/FileUploader.vb
+++ Parse.Data.UI/Parse.Data.UI/FileUploader.vb
@@ -6,10 +6,18 @@ Imports System.Web.Hosting
 Public Class FileUploader
     Implements IFileUploader
 
+    Private Shared Sub CreateDirectory(ByVal folder As String)
+        Dim directory = New DirectoryInfo(HostingEnvironment.MapPath(folder))
+        If Not directory.Exists Then
+            directory.Create()
+        End If
+    End Sub
+
     Public Function UploadImage(fileName As String, ByVal fileContent As Stream) As String Implements IFileUploader.UploadImage
         Dim filePath = String.Format("~/App_Data/Image/{0}_{1}", Guid.NewGuid(), fileName)
 
         Try
+            CreateDirectory("~/App_Data/Image/")
             Using destination = New FileStream(HostingEnvironment.MapPath(filePath), FileMode.Create)
                 fileContent.CopyTo(destination)
                 destination.Flush()
