1f28a02a97b6193bee33068afbd4e82a2a68f075
Gooner <suneeshchummar@live.com>
2012-10-07 02:48:02 -0400

    Reverted back the image preview change

--- Parse.Data.UI/Parse.Data.UI/GetLastImage.aspx.vb
+++ Parse.Data.UI/Parse.Data.UI/GetLastImage.aspx.vb
@@ -5,11 +5,21 @@ Public Class GetLastImage
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        'Dim folderPath = Server.MapPath(String.Format("~/App_Data/{0}/", Request("Directory")))
+        'Dim lastUpdatedFile = (New DirectoryInfo(folderPath)).GetFiles().OrderByDescending(Function(file) file.LastWriteTime).FirstOrDefault()
+        'If lastUpdatedFile IsNot Nothing Then
+        '    ImageLast.ImageUrl = VirtualPathUtility.ToAppRelative(lastUpdatedFile.FullName)
+        'End If
+
         Dim folderPath = Server.MapPath(String.Format("~/App_Data/{0}/", Request("Directory")))
         Dim lastUpdatedFile = (New DirectoryInfo(folderPath)).GetFiles().OrderByDescending(Function(file) file.LastWriteTime).FirstOrDefault()
-        If lastUpdatedFile IsNot Nothing Then
-            ImageLast.ImageUrl = String.Format("~/App_Data/{0}/{1}", Request("Directory"), lastUpdatedFile.Name)
-        End If
+        Try
+            Response.ContentType = "application/octet-stream"
+            Response.AppendHeader("Content-Disposition", "attachment; filename=" + lastUpdatedFile.Name)
+            Response.TransmitFile(lastUpdatedFile.FullName)
+            Response.End()
+        Catch ex As ThreadAbortException
+        End Try
     End Sub
 
 End Class
