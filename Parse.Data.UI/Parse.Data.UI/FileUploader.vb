6f0b29d8b6dd775364cd6af5da931a73e9eee864
Gooner <suneeshchummar@live.com>
2012-10-06 22:40:44 -0400

    Added service to upload file

---
+++ Parse.Data.UI/Parse.Data.UI/FileUploader.vb
@@ -0,0 +1,20 @@
+﻿Imports System.ServiceModel.Activation
+Imports System.IO
+
+<AspNetCompatibilityRequirements(RequirementsMode:=AspNetCompatibilityRequirementsMode.Allowed)>
+Public Class FileUploader
+    Implements IFileUploader
+
+    Public Function UploadImage(fileName As String, ByVal fileContent As Stream) As String Implements IFileUploader.UploadImage
+        Dim filePath = String.Format("~/App_Data/Image/{0}_{1}", Guid.NewGuid(), fileName)
+
+        Using destination = New FileStream(filePath, FileMode.Create)
+            fileContent.CopyTo(destination)
+            destination.Flush()
+            destination.Close()
+        End Using
+
+        Return filePath
+    End Function
+
+End Class
---
+++ Parse.Data.UI/Parse.Data.UI/Interfaces/IFileUploader.vb
@@ -0,0 +1,12 @@
+﻿Imports System.ServiceModel
+Imports System.ServiceModel.Web
+Imports System.IO
+
+<ServiceContract()>
+Public Interface IFileUploader
+
+    <OperationContract()>
+    <WebInvoke(Method:="POST", UriTemplate:="Image/{fileName}")>
+    Function UploadImage(ByVal fileName As String, ByVal fileContent As Stream) As String
+
+End Interface
